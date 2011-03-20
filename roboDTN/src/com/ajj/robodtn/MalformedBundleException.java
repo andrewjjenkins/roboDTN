f30ce1d2b41c5a35792b54d2150d56845e3c1515
Andrew Jenkins <andrew.jenkins@colorado.edu>
2011-03-19 18:35:59 -0600

    Make the MalformedBundleException allow the particular malformity to be specified.

--- roboDTN/src/com/ajj/robodtn/MalformedBundleException.java
+++ roboDTN/src/com/ajj/robodtn/MalformedBundleException.java
@@ -2,8 +2,19 @@ package com.ajj.robodtn;
 
 public class MalformedBundleException extends Exception {
 	private static final long serialVersionUID = 1870019398876452908L;
-	
+	private Malformity malformity;
+
 	MalformedBundleException(String s) {
 		super(s);
+		this.malformity = Malformity.UNSPECIFIED;
+	}
+
+	MalformedBundleException(Malformity malformity, String s) {
+		super(s);
+		this.malformity = malformity;
+	}
+
+	public Malformity getMalformity() {
+		return malformity;
 	}
 }
---
+++ roboDTN/src/com/ajj/robodtn/Malformity.java
@@ -0,0 +1,12 @@
+package com.ajj.robodtn;
+
+public enum Malformity {
+	UNSPECIFIED,		/* Exception creator didn't specify */
+	OTHER,				/* Creator was too lazy to define a new exception */
+	DICTTOOSHORT,		/* Dictionary was shorter than it could possibly be. */
+	EIDREFNOTINDICT,	/* The EID reference was outside the dictionary bounds. */
+	INVALIDVERSION,		/* The version wasn't 0x06 (RFC5050) */
+	NOLASTBLOCK,		/* There wasn't a block with the "last block" flag set. */
+	TOOMANYPAYLOADS,	/* There was more than one block with the payload type 0x00. */
+	TOOSHORT			/* The bundle wasn't as long as it was supposed to be. */
+}
