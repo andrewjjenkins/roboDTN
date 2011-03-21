93bf7f838d1c702207f71d8597fe3a856eb166c3
Andrew Jenkins <andrew.jenkins@colorado.edu>
2011-03-20 20:13:45 -0600

    Add EidPartReference class.

---
+++ roboDTN/src/com/ajj/robodtn/serialize/EidPartReference.java
@@ -0,0 +1,13 @@
+package com.ajj.robodtn.serialize;
+
+public class EidPartReference {
+	private long ref;
+	
+	public void set(long ref) {
+		this.ref = ref;
+	}
+	
+	public long get() {
+		return ref;
+	}
+}
