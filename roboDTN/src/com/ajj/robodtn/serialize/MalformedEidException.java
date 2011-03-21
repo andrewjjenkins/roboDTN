3634c6752e447426197d053180dd9ecf2e5aa62a
Andrew Jenkins <andrew.jenkins@colorado.edu>
2011-03-20 20:16:35 -0600

    Add MalformedEidException.

---
+++ roboDTN/src/com/ajj/robodtn/serialize/MalformedEidException.java
@@ -0,0 +1,9 @@
+package com.ajj.robodtn.serialize;
+
+public class MalformedEidException extends Exception {
+	public MalformedEidException(String string) {
+		super(string);
+	}
+
+	static final long serialVersionUID = 671939411140945808L;
+}
