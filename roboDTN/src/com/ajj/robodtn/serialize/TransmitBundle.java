1098ae4f6f34f234ab8588c83012773185de2d6f
Andrew Jenkins <andrew.jenkins@colorado.edu>
2011-03-21 21:36:17 -0600

    Add TransmitBundle, which serializes a bundle to an SdnvDataOutputStream.

---
+++ roboDTN/src/com/ajj/robodtn/serialize/TransmitBundle.java
@@ -0,0 +1,65 @@
+package com.ajj.robodtn.serialize;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import com.ajj.robodtn.Bundle;
+import com.ajj.robodtn.Sdnv;
+
+public class TransmitBundle extends SdnvDataOutputStream {
+	
+	public TransmitBundle(OutputStream out) {
+		super(out);
+	}
+
+	public void transmit(Bundle b) throws IOException, MalformedEidException {
+		/* The version and procFlags fields are not counted in blockLength,
+		 * so we can write them without counting their length. */
+		writeByte(Bundle.VERSION_RFC5050);
+		writeSdnv(b.procFlags);
+		
+		/* blockLength = sum of all PBB fields after blockLength, so we 
+		 * serialize these fields first, counting their length. */
+		int blockLength = 0;
+		ArrayList<byte []> fields = new ArrayList<byte []>();
+		SerializedDictionary sd = new SerializedDictionary(b);
+		fields.add(new Sdnv(sd.dst_so.get()).getBytes());
+		fields.add(new Sdnv(sd.dst_sspo.get()).getBytes());
+		fields.add(new Sdnv(sd.src_so.get()).getBytes());
+		fields.add(new Sdnv(sd.src_sspo.get()).getBytes());
+		fields.add(new Sdnv(sd.rptto_so.get()).getBytes());
+		fields.add(new Sdnv(sd.rptto_sspo.get()).getBytes());
+		fields.add(new Sdnv(sd.cust_so.get()).getBytes());
+		fields.add(new Sdnv(sd.cust_sspo.get()).getBytes());
+		fields.add(new Sdnv(((long)(b.createTimestamp.getTime()))/1000 - Bundle.DTNEPOCH).getBytes());
+		fields.add(new Sdnv(b.createSeq).getBytes());
+		fields.add(new Sdnv(b.lifetime).getBytes());
+		fields.add(new Sdnv(sd.bytes.length).getBytes());
+		fields.add(sd.bytes);
+		
+		if(b.aduLength != 0) {
+			fields.add(new Sdnv(b.fragOffset).getBytes());
+			fields.add(new Sdnv(b.aduLength).getBytes());
+		}
+		
+		/* Count the length of all the fields we added. */
+		for(Iterator<byte []> i = fields.iterator(); i.hasNext(); ) {
+			byte [] bytes = i.next();
+			blockLength += bytes.length;
+		}
+		
+		/* Write the block length. */
+		writeSdnv(new Sdnv(blockLength));
+		
+		/* Write the rest of the fields in the primary bundle block. */
+		for(Iterator<byte []> i = fields.iterator(); i.hasNext(); ) {
+			byte [] bytes = i.next();
+			write(bytes, 0, bytes.length);
+		}
+		
+		/* FIXME: Write the rest of the blocks. */
+	}
+
+}
---
+++ roboDTNTest/src/com/ajj/robodtn/test/TransmitBundleTest.java
@@ -0,0 +1,52 @@
+package com.ajj.robodtn.test;
+
+import java.io.IOException;
+
+import android.content.res.AssetManager;
+import android.test.InstrumentationTestCase;
+
+import com.ajj.robodtn.Bundle;
+import com.ajj.robodtn.dtnUtil;
+import com.ajj.robodtn.serialize.MalformedEidException;
+import com.ajj.robodtn.serialize.TransmitBundle;
+
+public class TransmitBundleTest extends InstrumentationTestCase {
+	private class TransmitTestPair {
+		public TransmitTestPair(String acqAsset, Bundle bundle) {
+			this.acqAsset = acqAsset;
+			this.bundle = bundle;
+		}
+		public String acqAsset;
+		public Bundle bundle;
+	}
+	
+	private final TransmitTestPair [] testpairs = {
+		new TransmitTestPair("testbundles/dtn2bundle",
+			new Bundle(0x90, "dtn://destination/app", "dtn://syme.dtn/source", 
+					"dtn://syme.dtn/source", "dtn:none", 
+					dtnUtil.iso8601ToDate("2011-01-30T02:24:16Z"),
+					2, 60, 0, 0)),
+		new TransmitTestPair("testbundles/ionbundle", 
+			new Bundle(0x94, "ipn:1.1", "ipn:1.0", "dtn:none", "dtn:none", 
+					dtnUtil.iso8601ToDate("2009-04-27T00:05:47Z"), 1, 300, 0, 0))
+
+	};
+	
+	public void testTransmits() {
+		TransmitTestPair tp;
+		AssetManager am = getInstrumentation().getContext().getAssets();
+		
+		for(int i = 0; i < testpairs.length; i++) {
+			tp = testpairs[i];
+			try {
+				ComparisonStream cmpstream = new ComparisonStream(am.open(tp.acqAsset));
+				TransmitBundle tb = new TransmitBundle(cmpstream);			
+				tb.transmit(tp.bundle);
+			} catch (IOException exc) {
+				fail("Streams not equal for bundle " + i + exc.toString());
+			} catch (MalformedEidException e) {
+				fail("Test bundle had malformed EID " + e.toString());
+			}
+		}
+	}
+}
